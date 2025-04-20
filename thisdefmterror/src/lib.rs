db214752df6ef11b62c998da808212f03e0e3ac6
oxabz <legmatt0@gmail.com>
2025-04-20 16:01:25 +0200

    Adding documentation to thisdefmterror

--- thisdefmterror/src/lib.rs
+++ thisdefmterror/src/lib.rs
@@ -1,9 +1,16 @@
 #![no_std]
+/*!
+ * A crate implementing utilities for defining error in a similar way to thiserror for the embedded space that use defmt.
+ * 
+ */
+
 
 pub use thisdefmterror_macros::DefmtError;
 
 /**
- * Trait that describe an error that supports printing via defmt and core formating
+ * Trait that describe an error that supports printing via defmt and core formating.
+ * 
+ * This crate also implements a derive similar to the crate this error
  */
 pub trait DefmtError: core::error::Error + defmt::Format {}
 
